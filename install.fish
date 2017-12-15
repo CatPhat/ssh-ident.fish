6a02dd6b07fd02f4e011f87bb9463c8261f84f35
Kian Movasagi <kian@vedos.io>
2017-12-15 12:47:08 -0700

    add private namespace function prefix

--- install.fish
+++ install.fish
@@ -7,21 +7,21 @@ end
 
 log Set ssh-ident path: $__ssh_ident_fish_bin
 
-function create_symlink
+function __ssh_ident_fish_create_symlink
     if not test -L $__ssh_ident_fish_bin_path/$argv
         ln -s $__ssh_ident_fish_bin $__ssh_ident_fish_bin_path/$argv
         log "Create symlink for $argv | $__ssh_ident_fish_bin_path/$argv -> $__ssh_ident_fish_bin"
     end
 end
 
-function create_home_bin
+function __ssh_ident_fish_create_home_bin
     if [ ! -d $__ssh_ident_fish_bin_path ]
         mkdir -p $__ssh_ident_fish_bin_path
         log mkdir $__ssh_ident_fish_bin_path
     end
 end
 
-function set_path
+function __ssh_ident_fish_set_path
     if not contains $__ssh_ident_fish_bin_path $fish_user_paths
         set -U fish_user_paths $__ssh_ident_fish_bin_path $fish_user_paths
         log Update \$fish_user_paths to include $__ssh_ident_fish_bin_path
@@ -29,8 +29,8 @@ function set_path
 end
 
 
-create_home_bin
-set_path
-create_symlink ssh
-create_symlink scp
-create_symlink sftp
+__ssh_ident_fish_create_home_bin
+__ssh_ident_fish_set_path
+__ssh_ident_fish_create_symlink ssh
+__ssh_ident_fish_create_symlink scp
+__ssh_ident_fish_create_symlink sftp
