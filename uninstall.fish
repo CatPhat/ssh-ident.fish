aa1281b28cd8ba4d240a0292192f627991bdbaab
Kian Movasagi <kian@vedos.io>
2017-12-15 15:47:08 -0700

    add uninstall.fish

--- uninstall.fish
+++ uninstall.fish
@@ -2,4 +2,16 @@ function log
     echo "[ssh-ident.fish]" $argv
 end
 
-function check_if_true_symbolic_link
+function __sif_remove_symbolic_links
+    set -l bin $argv[1]
+    set -l check_result (__sif_check_if_symbolic_link_is_valid $bin)
+    if test valid = $check_result 
+        set -l bin_path $__sif_home_bin_path/$bin
+        rm $bin_path
+        log removed $bin_path 
+    end
+end
+
+for bin in $__sif_bins
+    __sif_remove_symbolic_links $bin
+end
